6e18df449880942586f98491145d27eb38657fb1
Monika Grzanek <monski@o2.pl>
2021-11-30 23:33:41 +0100

    Add function initAccordion in js

--- src/js/script.js
+++ src/js/script.js
@@ -60,8 +60,9 @@
       thisProduct.data = data;
       
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       
-      console.log('new Product:', thisProduct)
+      console.log('new Product:', thisProduct);
     }
     renderInMenu(){
       const thisProduct = this;
@@ -78,6 +79,22 @@
       menuContainer.appendChild(thisProduct.element);
       console.log('show menuContainer:', menuContainer);
     }
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = classNames.menuProduct.wrapperActive;
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        /* toggle active class on thisProduct.element */
+    });
   }
 
   const app = {
