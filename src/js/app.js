a7cbabd040d27e67e9772bebe242a99c426dc5da
Monika Grzanek <monski@o2.pl>
2021-12-23 00:16:56 +0100

    Correct in components

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,52 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';  
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    //w ten sposób znaleźliśmy kontenery poszczególnych stron, ale powinniśmy przechowywać kontenery poszczególnych podstron, czyli mieć wiele elementów, dlatego dodajemy 'children'
+    // w ten sposób otrzymamy dzieci kontenera stron #pages
+    // aby aktywowała się pierwsza z podstron używamy metody activatePage
+    // metodę tą wywołamy na kontenerze stron i przekażemy pierwszy otrzymany element 
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+    console.log('show idFromHash', idFromHash);
+    thisApp.activatePage(idFromHash); 
+    // teraz musimy dodać nasłuchiwacz do każdego linku
+    for(let link in thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this; //zazwyczaj w handlerze eventu wpisujemy definicję stałej w której zapisujemy obiekt this
+        event.preventDefault();
+        /* get id from href */
+        const id = clickedElement.getAttribute('href').replace('#', ''); // ponieważ href zawiera #, którego nie zawiera id strony to musimy go zamienić na pusty ciąg znaków
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+        // Aby po odświeżeniu strony nie przenosiło nas do domyślnie ustawionej zakładki należy dodać funkcjonalność zmieny adresów url w zależności od podstrony na której aktualnie się znajdujemy
+        /* change url hash */
+        window.location.hash = '#/' + id; // aby po zmianie zakładki strona nie została przewinięta należy dodać po hashu '/'
+      });
+    }
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    //Funkcja ta powinna nadać odpowiedniemu wrapperowi klasę active
+    // Powinna też wyróżninać zakładkę strony aktualnie wyświetlanej
+
+    /* add class active to matching pages, remove from non-matching pages */
+    for(let page in thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId); // w toggle pierwszym argumentem jest podanie nazwy klasy, a w drugim argumencie warunek jaki musi spełniać 
+    }
+    /* add class active to matching links, remove from non-matching links */
+    for(let link in thisApp.navLinks){ // dla każdego z linków w thisApp.navLinks chcemy dodać lub usunąć klasę active w zależności od tego czy atrybut href tego linka jest równy id podanej podstrony
+      link.classList.toggle(
+        classNames.pages.active, 
+        link.getAttribute('href') == '#' + pageId
+      ); // w toggle pierwszym argumentem jest podanie nazwy klasy, a w drugim argumencie warunek jaki musi spełniać 
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
 
