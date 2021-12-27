56ccbb613830fd1a9a5a2546109721c19b6a02cd
Monika Grzanek <monski@o2.pl>
2021-12-28 00:31:11 +0100

    Add initWidgets in Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -12,17 +12,25 @@ class Booking{
   render(element){
     const thisBooking = this;
 
-    const generateHTML = templates.bookingWidget;
+    const generateHTML = templates.bookingWidget();
     console.log('show booking generateHTML', generateHTML);
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = generateHTML;
-    thisBooking.dom.peopleAmount = select.booking.peopleAmount;
-    thisBooking.dom.hoursAmount = select.booking.hoursAmount;
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
   }
 
   initWidgets(){
+    const thisBooking = this;
     
+    thisBooking.amountWidget = [];
+    thisBooking.amountWidget.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+    /*thisBooking.amountWidget.addEventListener('updated', function(){
+
+    });*/
   }
 }
 
