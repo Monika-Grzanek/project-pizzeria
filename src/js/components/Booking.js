8a2b7c8937a45a9d59d74e161dd425bf019b2f0a
Monika Grzanek <monski@o2.pl>
2021-12-28 00:10:39 +0100

    Add render in Booking in src.js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,3 +1,30 @@
-import {settings, select, classNames, templates} from './settings.js';
+import {/*settings, classNames,*/ select, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking{
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element){
+    const thisBooking = this;
+
+    const generateHTML = templates.bookingWidget;
+    console.log('show booking generateHTML', generateHTML);
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generateHTML;
+    thisBooking.dom.peopleAmount = select.booking.peopleAmount;
+    thisBooking.dom.hoursAmount = select.booking.hoursAmount;
+  }
+
+  initWidgets(){
+    
+  }
+}
+
 
 export default Booking;
