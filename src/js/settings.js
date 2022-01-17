3428b70d232829afee5493576ec3ee6df86de9a0
Monika Grzanek <monski@o2.pl>
2022-01-17 13:24:19 +0100

    Change in settings.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -131,7 +131,7 @@ export const settings = {
     products: 'products',
     orders: 'orders',
     // eslint-disable-next-line no-dupe-keys
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'bookings',
